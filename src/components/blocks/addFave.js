426e88b65bd3dbcacc160705fb772a11ee4e423b
James Sample <jamesgsample@gmail.com>
2023-03-11 12:46:04 +0000

    working on adding fave

--- src/components/blocks/addFave.js
+++ src/components/blocks/addFave.js
@@ -4,8 +4,6 @@ import axios from "axios";
 const AddFave = ({ station, user, profile }) => {
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(station);
-    console.log(profile.email);
     axios.post("http://localhost:4000/insert", {
       user: profile.email,
       Favourite: station,
